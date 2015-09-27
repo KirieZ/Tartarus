e53563202df6fcf5c8f15bddaccd0f05be7af391
KirieZ <guilherme_kiriez@hotmail.com>
2015-09-27 00:34:02 -0300

    Added MySQL conversion for arcadia tables. Follow up to @6cdf67023

---
+++ sql-files/MySQL/game/AuctionCategoryResource.sql
@@ -0,0 +1,47 @@
+CREATE TABLE IF NOT EXISTS `AuctionCategoryResource` (
+	`category_id` int(11) NOT NULL,
+	`sub_category_id` int(11) NOT NULL,
+	`name_id` int(11) NOT NULL,
+	`local_flag` int(11) NOT NULL,
+	`item_group` int(11) NOT NULL,
+	`item_class` int(11) NOT NULL
+) ENGINE=MyISAM;
+
+REPLACE INTO `AuctionCategoryResource` VALUES ('0', '-1', '6700', '0', '1', '-1');
+REPLACE INTO `AuctionCategoryResource` VALUES ('0', '0', '6800', '0', '1', '101');
+REPLACE INTO `AuctionCategoryResource` VALUES ('0', '1', '6801', '0', '1', '102');
+REPLACE INTO `AuctionCategoryResource` VALUES ('0', '2', '6802', '0', '1', '103');
+REPLACE INTO `AuctionCategoryResource` VALUES ('0', '3', '6803', '0', '1', '104');
+REPLACE INTO `AuctionCategoryResource` VALUES ('0', '4', '6812', '0', '1', '113');
+REPLACE INTO `AuctionCategoryResource` VALUES ('0', '5', '6804', '0', '1', '105');
+REPLACE INTO `AuctionCategoryResource` VALUES ('0', '6', '6805', '0', '1', '106');
+REPLACE INTO `AuctionCategoryResource` VALUES ('0', '7', '6806', '0', '1', '107');
+REPLACE INTO `AuctionCategoryResource` VALUES ('0', '8', '6807', '0', '1', '108');
+REPLACE INTO `AuctionCategoryResource` VALUES ('0', '9', '6809', '0', '1', '110');
+REPLACE INTO `AuctionCategoryResource` VALUES ('0', '10', '6810', '0', '1', '111');
+REPLACE INTO `AuctionCategoryResource` VALUES ('0', '11', '6811', '0', '1', '112');
+REPLACE INTO `AuctionCategoryResource` VALUES ('1', '-1', '6701', '0', '2', '-1');
+REPLACE INTO `AuctionCategoryResource` VALUES ('1', '0', '6814', '0', '2', '201');
+REPLACE INTO `AuctionCategoryResource` VALUES ('1', '1', '6815', '0', '2', '202');
+REPLACE INTO `AuctionCategoryResource` VALUES ('1', '2', '6816', '0', '2', '203');
+REPLACE INTO `AuctionCategoryResource` VALUES ('1', '3', '6817', '0', '2', '204');
+REPLACE INTO `AuctionCategoryResource` VALUES ('2', '-1', '6702', '0', '3', '-1');
+REPLACE INTO `AuctionCategoryResource` VALUES ('3', '-1', '6703', '0', '4', '-1');
+REPLACE INTO `AuctionCategoryResource` VALUES ('4', '-1', '6704', '0', '5', '-1');
+REPLACE INTO `AuctionCategoryResource` VALUES ('5', '-1', '6705', '0', '6', '-1');
+REPLACE INTO `AuctionCategoryResource` VALUES ('6', '-1', '6706', '0', '7', '-1');
+REPLACE INTO `AuctionCategoryResource` VALUES ('7', '-1', '6707', '0', '8', '-1');
+REPLACE INTO `AuctionCategoryResource` VALUES ('8', '-1', '6708', '0', '9', '-1');
+REPLACE INTO `AuctionCategoryResource` VALUES ('8', '0', '6818', '0', '9', '301');
+REPLACE INTO `AuctionCategoryResource` VALUES ('8', '1', '6819', '0', '9', '302');
+REPLACE INTO `AuctionCategoryResource` VALUES ('8', '2', '6820', '0', '9', '303');
+REPLACE INTO `AuctionCategoryResource` VALUES ('9', '-1', '6719', '0', '110', '-1');
+REPLACE INTO `AuctionCategoryResource` VALUES ('10', '-1', '6709', '0', '10', '-1');
+REPLACE INTO `AuctionCategoryResource` VALUES ('11', '-1', '6710', '0', '13', '-1');
+REPLACE INTO `AuctionCategoryResource` VALUES ('12', '-1', '6711', '0', '17', '-1');
+REPLACE INTO `AuctionCategoryResource` VALUES ('13', '-1', '6712', '0', '21', '-1');
+REPLACE INTO `AuctionCategoryResource` VALUES ('14', '-1', '6713', '0', '22', '-1');
+REPLACE INTO `AuctionCategoryResource` VALUES ('15', '-1', '6714', '0', '23', '-1');
+REPLACE INTO `AuctionCategoryResource` VALUES ('16', '-1', '6716', '0', '93', '-1');
+REPLACE INTO `AuctionCategoryResource` VALUES ('17', '-1', '6720', '0', '97', '-1');
+REPLACE INTO `AuctionCategoryResource` VALUES ('18', '-1', '6717', '0', '99', '-1');
