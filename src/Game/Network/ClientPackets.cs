74f06379d3af2a05ec97a0555e11332c481bce16
KirieZ <guilherme_kiriez@hotmail.com>
2015-10-03 14:37:30 -0300

    Partial addition of lobby packets

--- src/Game/Network/ClientPackets.cs
+++ src/Game/Network/ClientPackets.cs
@@ -26,11 +26,17 @@ namespace Game.Network
 			PacketsDb = new Dictionary<ushort, PacketAction>();
 
 			#region Packets
-			//PacketsDb.Add(0x07D5, 
+			PacketsDb.Add(0x0032, CS_Version);
+			PacketsDb.Add(0x07D5, CS_AccountWithAuth);
+			PacketsDb.Add(0x1F40, CS_SystemSpecs);
+			PacketsDb.Add(0x07D1, CS_CharacterList);
+			PacketsDb.Add(0x07D2, CS_CreateCharacter);
+			PacketsDb.Add(0x07D3, CS_DeleteCharacter);
+			PacketsDb.Add(0x07D6, CS_CheckCharacterName);
 			#endregion
 		}
 
-
+		
 		/// <summary>
 		/// Called whenever a packet is received from a game client
 		/// </summary>
@@ -49,13 +55,109 @@ namespace Game.Network
 			Task.Factory.StartNew(() => { PacketsDb[stream.GetId()].Invoke(client, stream); });
 		}
 
-
 		#region Client Packets
-		
+		#region Unused Packets
+		/// <summary>
+		/// SFrame version (not used officially)
+		/// </summary>
+		/// <param name="client"></param>
+		/// <param name="stream"></param>
+		private void CS_Version(GameClient client, PacketStream stream)
+		{
+			//string version = stream.ReadString(20);
+		}
+
+		/// <summary>
+		/// System specs (official name not found)
+		/// not used
+		/// </summary>
+		/// <param name="client"></param>
+		/// <param name="stream"></param>
+		private void CS_SystemSpecs(GameClient client, PacketStream stream) { }
+		#endregion
+
+		#region Lobby
+		/// <summary>
+		/// Connection from auth
+		/// </summary>
+		/// <param name="client"></param>
+		/// <param name="stream"></param>
+		private void CS_AccountWithAuth(GameClient client, PacketStream stream)
+		{
+			string userId = stream.ReadString(61);
+			ulong key = stream.ReadUInt64();
+		}
+
+		/// <summary>
+		/// Request the character list
+		/// </summary>
+		/// <param name="client"></param>
+		/// <param name="stream"></param>
+		private void CS_CharacterList(GameClient client, PacketStream stream)
+		{
+			string account = stream.ReadString(61);
+		}
+
+		/// <summary>
+		/// Checks if character name is in use
+		/// </summary>
+		/// <param name="client"></param>
+		/// <param name="stream"></param>
+		private void CS_CheckCharacterName(GameClient client, PacketStream stream)
+		{
+			string name = stream.ReadString(19);
+		}
+
+		/// <summary>
+		/// Request to create a character
+		/// </summary>
+		/// <param name="client"></param>
+		/// <param name="stream"></param>
+		private void CS_CreateCharacter(GameClient client, PacketStream stream)
+		{
+			// Lobby_Character_Info
+		}
+
+		/// <summary>
+		/// Requests to delete a character
+		/// </summary>
+		/// <param name="client"></param>
+		/// <param name="stream"></param>
+		private void CS_DeleteCharacter(GameClient client, PacketStream stream)
+		{
+			string name = stream.ReadString(19);
+			//string securityCode = stream.ReadString(19);
+		}
+		#endregion
+
 		#endregion
 
 		#region Server Packets
-		
+		/// <summary>
+		/// Sends a response to a packet
+		/// </summary>
+		/// <param name="client"></param>
+		/// <param name="packetId"></param>
+		/// <param name="response"></param>
+		/// <param name="value"></param>
+		public void Response(GameClient client, ushort packetId, ushort response = 0, int value = 0)
+		{
+			PacketStream stream = new PacketStream(0x0000);
+			
+			stream.WriteUInt16(packetId);
+			stream.WriteUInt16(response);
+			stream.WriteInt32(value);
+
+			ClientManager.Instance.Send(client, stream);
+		}
+
+		public void CharacterList(GameClient client)
+		{
+			//uint currentSvTime
+			//ushort last_login_index
+			//ushort count
+			//Lobby_Character_Info[count];
+		}
 		#endregion
 	}
 }
