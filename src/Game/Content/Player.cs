9959c94b3309b37aa1073e401a8c0bc7ed63de3e
KirieZ <guilherme_kiriez@hotmail.com>
2015-10-06 18:38:06 -0300

    Cleaned up Content/Player.cs

--- src/Game/Content/Player.cs
+++ src/Game/Content/Player.cs
@@ -9,74 +9,10 @@ using Game.Players;
 
 namespace Game.Content
 {
-    [Flags]
-    public enum JobDepth
-    {
-        Basic = 1,
-        First = 2,
-        Second = 4,
-        Master = 8
-    }
-
-    public class JobDBEntry
-    {
-        public int MaxLevel { get; set; }
-        public JobDepth JobDepth { get; set; }
-
-        public float StrMult { get; set; }
-        public float VitMult { get; set; }
-        public float DexMult { get; set; }
-        public float AgiMult { get; set; }
-        public float IntMult { get; set; }
-        public float WisMult { get; set; }
-        public float LuckMult { get; set; }
-    }
-
 	public partial class Player
 	{
 		static int sqlConType = Settings.SqlEngine;
-        static string sqlConString = "Server="+Settings.SqlGameIp+";Database="+Settings.SqlGameDatabase+";UID="+Settings.SqlGameUsername+";PWD="+Settings.SqlGamePassword+";Connection Timeout=5;";
-		static string sqlUConString = "Server=" + Settings.SqlUserIp + ";Database=" + Settings.SqlUserDatabase + ";UID=" + Settings.SqlUserUsername + ";PWD=" + Settings.SqlUserPassword + ";Connection Timeout=5;";
-
-		/// <summary>
-        /// Table storing EXP-TNL (To-Next-Level) values
-        /// </summary>
-        public static long[] ExpTable;
-        /// <summary>
-        /// Table storing JP-TNJL (To-Next-JLevel) values for job_0
-        /// </summary>
-        public static int[] Jp0Table;
-        /// <summary>
-        /// Table storing JP-TNJL (To-Next-JLevel) values for job_1
-        /// </summary>
-        public static int[] Jp1Table;
-        /// <summary>
-        /// Table storing JP-TNJL (To-Next-JLevel) values for job_2
-        /// </summary>
-        public static int[] Jp2Table;
-        /// <summary>
-        /// Table storing JP-TNJL (To-Next-JLevel) values for job_3
-        /// </summary>
-        public static int[] Jp3Table;
-
-        public static Dictionary<int, JobDBEntry> JobDB;
-
-        static internal void LoadLvDB()
-        {
-        }
-
-        static internal void LoadJobDB()
-        {
-
-        }
-
-        /// <summary>
-        /// Loads the Player related tables into memory
-        /// </summary>
-        public static void Start()
-        {
-        }
-
+        static string sqlConString = "Server=" + Settings.SqlUserIp + ";Database=" + Settings.SqlUserDatabase + ";UID=" + Settings.SqlUserUsername + ";PWD=" + Settings.SqlUserPassword + ";Connection Timeout=5;";
 
 		/// <summary>
 		/// Connection data
@@ -105,7 +41,7 @@ namespace Game.Content
 		{
 			List<LobbyCharacterInfo> charList = new List<LobbyCharacterInfo>();
 
-			using (DBManager dbManager = new DBManager(sqlConType, sqlUConString))
+			using (DBManager dbManager = new DBManager(sqlConType, sqlConString))
 			{
 				using (DbCommand dbCmd = dbManager.CreateCommand("SELECT * FROM Characters WHERE account_id = @accId AND delete_date > @now LIMIT 5"))
 				{
